db8a08292768f156557f7c3086a6471536d6f7c5
Milos Savic <msavic248@yahoo.com>
2022-11-11 18:04:53 +0100

    Added replay function

--- main.js
+++ main.js
@@ -1,90 +1,93 @@
 import './style.css'
 import { mediaCounter } from './mediaCounter.js'
 
-const letters = [];
-const app = document.querySelector('#app');
+export default function lettersGame() {
+  const letters = [];
+  const app = document.querySelector('#app');
 
-app.innerHTML = `
-    <span id="p"><button id="pButton">P</button></span>
-    <span id="o1"><button id="o1Button">O</button></span>
-    <span id="s"><button id="sButton">S</button></span>
-    <span id="a"><button id="aButton">A</button></span>
-    <span id="o2"><button id="o2Button">O</button></span>
-    <button class="btn">Start</button>
-    <p class="counter"></p>
-`
+  app.innerHTML = `
+      <span id="p"><button id="pButton">P</button></span>
+      <span id="o1"><button id="o1Button">O</button></span>
+      <span id="s"><button id="sButton">S</button></span>
+      <span id="a"><button id="aButton">A</button></span>
+      <span id="o2"><button id="o2Button">O</button></span>
+      <button class="btn">Start</button>
+      <p class="counter"></p>
+  `
 
-const spans = document.querySelectorAll("span");
-const p = document.querySelector("#p");
-const pButton = document.querySelector("#pButton");
-const o1 = document.querySelector("#o1");
-const o1Button = document.querySelector("#o1Button");
-const s = document.querySelector("#s");
-const sButton = document.querySelector("#sButton");
-const a = document.querySelector("#a");
-const aButton = document.querySelector("#aButton");
-const o2 = document.querySelector("#o2");
-const o2Button = document.querySelector("#o2Button");
+  const spans = document.querySelectorAll("span");
+  const p = document.querySelector("#p");
+  const pButton = document.querySelector("#pButton");
+  const o1 = document.querySelector("#o1");
+  const o1Button = document.querySelector("#o1Button");
+  const s = document.querySelector("#s");
+  const sButton = document.querySelector("#sButton");
+  const a = document.querySelector("#a");
+  const aButton = document.querySelector("#aButton");
+  const o2 = document.querySelector("#o2");
+  const o2Button = document.querySelector("#o2Button");
 
-const start = document.querySelector(".btn");
-const counter = document.querySelector(".counter");
+  const start = document.querySelector(".btn");
+  const counter = document.querySelector(".counter");
 
-//Game start
-start.addEventListener("click", () => {
+  //Game start
+  start.addEventListener("click", () => {
 
-  //remove flex and hides start button
-  app.classList.remove("flex");
-  start.classList.add("hide");
+    //remove flex and hides start button
+    app.classList.remove("flex");
+    start.classList.add("hide");
 
-  //P button
-  p.addEventListener("click", () => {
-    letters.push("P")
+    //P button
+    p.addEventListener("click", () => {
+      letters.push("P")
 
-    //hides and disables button
-    p.classList.add("transparent");
-    pButton.classList.add("cursor");
-    pButton.setAttribute("disabled", "disabled");
+      //hides and disables button
+      p.classList.add("transparent");
+      pButton.classList.add("cursor");
+      pButton.setAttribute("disabled", "disabled");
+    })
+
+    //First O button
+    o1.addEventListener("click", () => {
+      letters.push("O")
+
+      o1.classList.add("transparent");
+      o1Button.classList.add("cursor");
+      o1Button.setAttribute("disabled", "disabled");
+    })
+
+    //S button
+    s.addEventListener("click", () => {
+      letters.push("S")
+
+      s.classList.add("transparent");
+      sButton.classList.add("cursor");
+      sButton.setAttribute("disabled", "disabled");
+    })
+
+    //A button
+    a.addEventListener("click", () => {
+      letters.push("A")
+
+      a.classList.add("transparent");
+      aButton.classList.add("cursor");
+      aButton.setAttribute("disabled", "disabled");
+    })
+
+    //Second O button
+    o2.addEventListener("click", () => {
+      letters.push("O")
+
+      o2.classList.add("transparent");
+      o2Button.classList.add("cursor");
+      o2Button.setAttribute("disabled", "disabled");
+    })
+
+    //timer and randomizer
+    mediaCounter(counter, app, spans, letters);
+    
   })
+}
 
-  //First O button
-  o1.addEventListener("click", () => {
-    letters.push("O")
-
-    o1.classList.add("transparent");
-    o1Button.classList.add("cursor");
-    o1Button.setAttribute("disabled", "disabled");
-  })
-
-  //S button
-  s.addEventListener("click", () => {
-    letters.push("S")
-
-    s.classList.add("transparent");
-    sButton.classList.add("cursor");
-    sButton.setAttribute("disabled", "disabled");
-  })
-
-  //A button
-  a.addEventListener("click", () => {
-    letters.push("A")
-
-    a.classList.add("transparent");
-    aButton.classList.add("cursor");
-    aButton.setAttribute("disabled", "disabled");
-  })
-
-  //Second O button
-  o2.addEventListener("click", () => {
-    letters.push("O")
-
-    o2.classList.add("transparent");
-    o2Button.classList.add("cursor");
-    o2Button.setAttribute("disabled", "disabled");
-  })
-
-  //timer and randomizer
-  mediaCounter(counter, app, spans, letters);
-  
-})
-
+lettersGame();
 
--- mediaCounter.js
+++ mediaCounter.js
@@ -1,3 +1,5 @@
+import lettersGame from "./main";
+
 export function mediaCounter(counter, app, spans, letters) {
 
   //media query for above 576px
@@ -11,8 +13,8 @@ export function mediaCounter(counter, app, spans, letters) {
   }
 
   function randomSpans(pc) {
-    const winWidth = app.clientWidth;
-    const winHeight = app.clientHeight;
+    // const winWidth = app.clientWidth;
+    // const winHeight = app.clientHeight;
 
     for ( let i = 0; i < spans.length; i++ ) {
     
@@ -24,7 +26,7 @@ export function mediaCounter(counter, app, spans, letters) {
       // get random numbers for each element
       if(pc){
         randomTop = getRandomNumber(0, 80);
-        randomLeft = getRandomNumber(0, 80);
+        randomLeft = getRandomNumber(0, 85);
       } else {
         randomTop = getRandomNumber(0, 200);
         randomLeft = getRandomNumber(0, 85);
@@ -43,6 +45,17 @@ export function mediaCounter(counter, app, spans, letters) {
     let pc;
     let count;
     let originalCount;
+
+    const p = document.querySelector("#p");
+    const pButton = document.querySelector("#pButton");
+    const o1 = document.querySelector("#o1");
+    const o1Button = document.querySelector("#o1Button");
+    const s = document.querySelector("#s");
+    const sButton = document.querySelector("#sButton");
+    const a = document.querySelector("#a");
+    const aButton = document.querySelector("#aButton");
+    const o2 = document.querySelector("#o2");
+    const o2Button = document.querySelector("#o2Button");
 
     if (event.matches) {
       console.log("PC Mode")
@@ -72,14 +85,45 @@ export function mediaCounter(counter, app, spans, letters) {
 
       if(win){
         clearInterval(timerId)
-        app.innerHTML = `<h1>Bravo!</h1>
-        <button class="btn">Igraj ponovo</button>`
+        app.innerHTML = `<div class="bravo">
+          <h1>Bravo!</h1>
+          <button class="btn">Igraj ponovo</button>
+        </div>`
+
+        //play again button resets the game
+        const again = document.querySelector(".btn");
+
+        again.addEventListener("click", ()=> {
+          lettersGame();
+          app.classList.add("flex");
+        })
       }
 
       //when count reaches 0, reset timer, randomize spans
       if(count < 0) {
         count = originalCount;
         letters.length = 0;
+
+        p.classList.remove("transparent");
+        pButton.classList.remove("cursor");
+        pButton.removeAttribute("disabled");
+
+        o1.classList.remove("transparent");
+        o1Button.classList.remove("cursor");
+        o1Button.removeAttribute("disabled");
+
+        s.classList.remove("transparent");
+        sButton.classList.remove("cursor");
+        sButton.removeAttribute("disabled");
+
+        a.classList.remove("transparent");
+        aButton.classList.remove("cursor");
+        aButton.removeAttribute("disabled");
+
+        o2.classList.remove("transparent");
+        o2Button.classList.remove("cursor");
+        o2Button.removeAttribute("disabled");
+
         randomSpans(pc);
       }
     }
